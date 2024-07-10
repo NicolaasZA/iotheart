1f818ef2b85d7ffbf3d0a8405ab4d5c5655a5ddb
NicolaasZA <lpretorius1995@gmail.com>
2024-07-10 21:03:46 +0200

    add system info

--- client/iotheart_client.py
+++ client/iotheart_client.py
@@ -1,4 +1,5 @@
 from scheduling import RepeatedTimer
+import psutil
 import requests
 import logging
 import time
@@ -8,9 +9,25 @@ target_ip = 'server_ip_here'
 device_id = 'device_id_here'
 beat_frequency_seconds = 10
 
+# Calculated
+beat_timeout = (beat_frequency_seconds - 1) * 1000
+beat_url = f'http://{target_ip}:1111/api/beat'
 
 def send_heartbeat(*args, **kwargs):
-    result = requests.post(f'http://{target_ip}:1111/api/beat?device_id={device_id}', data={}, timeout=20000).status_code
+
+    memory_info = psutil.virtual_memory()
+    disk_info = psutil.disk_usage('/')
+
+    payload = {
+        'device_id': device_id,
+        'mem_total': memory_info.total,
+        'mem_used': memory_info.used,
+        'mem_free': memory_info.available,
+        'disk_total': disk_info.total,
+        'disk_used': disk_info.used,
+        'disk_free': disk_info.free,
+    }
+    result = requests.post(url=beat_url, data=payload, timeout=beat_timeout).status_code
     if result != 200:
         print(f'Failed to send heartbeat. Response code is {result}')
     else:
--- server/iotheart_server.py
+++ server/iotheart_server.py
@@ -7,16 +7,29 @@ from bottle import Bottle, request, abort
 
 app = Bottle()
 
+sql = """
+update heartbeat
+set
+    beat_datetime_utc = now(),
+    mem_total = %s,
+    mem_used = %s,
+    mem_free = %s,
+    disk_total = %s,
+    disk_used = %s,
+    disk_free = %s
+where device_id = %s;
+"""
+
 
 def connect_db():
-    return mysql.connector.connect(host="db_host", user="db_user", database="db_name", password="db_password")
+    return mysql.connector.connect(host="server_ip_here", user="iotheart_server", database="iotheart", password="75#fGh3Xc&mf254")
 
 
-def add_heartbeat_to_db(device_id: str):
+def add_heartbeat_to_db(device_id: str, mt, mu, mf, dt, du, df):
     db = connect_db()
     cur = db.cursor()
 
-    cur.execute('update heartbeat set beat_datetime_utc = now() where device_id = %s;', (device_id,))
+    cur.execute(sql, (mt, mu, mf, dt, du, df, device_id))
 
     db.commit()
     cur.close()
@@ -24,11 +37,18 @@ def add_heartbeat_to_db(device_id: str):
 
 
 def receive_heartbeat():
-    device_id: str = request.params.get('device_id')
+    device_id: str = request.params.device_id
+    mem_total: int = request.params.mem_total
+    mem_used: int = request.params.mem_used
+    mem_free: int = request.params.mem_free
+    disk_total: int = request.params.disk_total
+    disk_used: int = request.params.disk_used
+    disk_free: int = request.params.disk_free
+
     if not device_id:
         abort(400, 'missing device_id')
         return
-    add_heartbeat_to_db(device_id)
+    add_heartbeat_to_db(device_id, mem_total, mem_used, mem_free, disk_total, disk_used, disk_free)
 
 
 def stop(sig=0, frame=None):
